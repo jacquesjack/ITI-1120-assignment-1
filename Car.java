cfe75f2d11ae1b0e3760e387dd60364be914d4f8
Jacques O'Gallagher-Roy <jack23gallag@gmail.com>
2022-01-30 14:44:50 -0500

    message

---
+++ Car.java
@@ -0,0 +1,82 @@
+/**
+ * @author Mehrdad Sabetzadeh, University of Ottawa
+ */
+public class Car {
+
+	/**
+	 * Instance variable for storing the type of the car
+	 */
+	private CarType type;
+
+	/**
+	 * Instance variable for storing the car plate number
+	 */
+	private String plateNum;
+
+	/**
+	 * @return the type of this car
+	 */
+	public CarType getType() {
+		// WRITE YOUR CODE HERE!
+	
+		return type; // REMOVE THIS STATEMENT AFTER IMPLEMENTING THIS METHOD
+	}
+
+	/**
+	 * Sets the type of the car
+	 * 
+	 * @param type is the car type
+	 */
+	public void setType(CarType type) {
+		// WRITE YOUR CODE HERE!
+		this.type = type;
+	}
+
+	/**
+	 * @return the plate number
+	 */
+	public String getPlateNum() {
+		// WRITE YOUR CODE HERE!
+		return plateNum; // REMOVE THIS STATEMENT AFTER IMPLEMENTING THIS METHOD
+	}
+
+	/**
+	 * Sets the car plate number
+	 * 
+	 * @param plateNum is the car plate number
+	 */
+	public void setPlateNum(String plateNum) {
+	// WRITE YOUR CODE HERE!
+		this.plateNum = plateNum;
+	}
+
+	/**
+	 * Constructor for Car
+	 * 
+	 * @param type     is the type of the car
+	 * @param plateNum is the car plate number
+	 */
+	public Car(CarType type, String plateNum) {
+	// WRITE YOUR CODE HERE!
+		this.type = type;
+		this.plateNum = plateNum;
+	}
+
+	/**
+	 * Returns a string representation of the car
+	 */
+	public String toString() {
+		// NOTE: The implementation of this method is complete. You do NOT need to
+		// change it for the assignment.
+		return Util.getLabelByCarType(type) + '(' + plateNum + ')';
+	}
+	public static void main(String[] args){
+		Car carOne = new Car(CarType.LARGE, "C1");
+		carOne.setType(CarType.LARGE);
+		carOne.setPlateNum("C2");
+		carOne.toString();
+		System.out.println(carOne.getPlateNum());
+ 		
+
+	}
+}
