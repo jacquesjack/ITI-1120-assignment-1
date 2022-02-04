8539e2852989da95339967730aba07341cea2b81
staticvortx <97464911+staticvortx@users.noreply.github.com>
2022-02-04 12:16:01 -0500

    Add files via upload

--- StudentInfo.java
+++ StudentInfo.java
@@ -16,7 +16,7 @@ public class StudentInfo {
     public static void display() {
 
         System.out.println("************************************************************");
-        System.out.println("*                                                          *");
+        System.out.println("*Raphael Daoust-Reid (300026960)                           *");
         System.out.println("*                                                          *");
         System.out.println("*                                                          *");
         System.out.println("*                                                          *");
